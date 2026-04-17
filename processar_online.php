34a7f9f43fc0776d77a55987f165fbddc914997d
rudecrow <48327214+rudecrow@users.noreply.github.com>
2026-04-17 10:45:47 +0100

    Add files via upload

--- processar_online.php
+++ processar_online.php
@@ -4,9 +4,6 @@
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // mysqli lança exceções
-
-<?php declare(strict_types=1);
-
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
